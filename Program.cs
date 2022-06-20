25ec0cf7fd549fd8c8383c50d4268e07a39edc45
Greg Doud <gpdoud@gmail.com>
2022-06-20 15:56:09 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Hello, World!");
+
+int add(int i, int j) => i + j;
+
+int sub(int i, int j) => i - j;
+
+int mult(int i, int j) => i * j;
+
+int div(int i, int j) => i / j;
+
+int mod(int i, int j) => i - (i / j * j);
+
+intFn[] funcs = { add, sub, mult, div, mod };
+
+foreach(intFn fn in funcs) {
+    Console.WriteLine($"{fn(12, 3)}");
+}
+
+intFn Rebecca = mod;
+Console.WriteLine(Rebecca(7, 3));
+
+
+delegate int intFn(int i, int j);
