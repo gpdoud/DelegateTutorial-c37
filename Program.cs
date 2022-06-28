193ea06173e8f03206aa0cecc5d9a981a8fd9c9a
Greg Doud <gpdoud@gmail.com>
2022-06-28 14:23:09 -0400

    Final commit

--- Program.cs
+++ Program.cs
@@ -1,23 +1,68 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Ready!");
 
-int add(int i, int j) => i + j;
+Main.Run();
 
-int sub(int i, int j) => i - j;
+// this class is the pubhlisher
+// after Start is called, it will wait
+// 5 seconds then publish an event
+class EmergencyAlertCenter {
 
-int mult(int i, int j) => i * j;
+    public event AlertEvent AlertHandler;
 
-int div(int i, int j) => i / j;
+    public void Start() {
+        // sleep for 5 seconds
+        Thread.Sleep(5000);
+        // custom data being send to all subscribers
+        AlertArgs args = new AlertArgs {
+            Code = 100, Message = $"The message is for code 100."
+        };
+        PublishEvent(args);
+    }
 
-int mod(int i, int j) => i - (i / j * j);
+    protected virtual void PublishEvent(AlertArgs args) {
+        //if AlertHandler is not null then calls all delegates
+        Console.WriteLine("Publish event ...");
+        // publish message to all subscribers
+        AlertHandler?.DynamicInvoke(null, args);
+    }
 
-intFn[] funcs = { add, sub, mult, div, mod };
-
-foreach(intFn fn in funcs) {
-    Console.WriteLine($"{fn(12, 3)}");
 }
 
-intFn Rebecca = mod;
-Console.WriteLine(Rebecca(7, 3));
+// this class is the subscriber
+// it has two delegates that will 
+// receive the published message
+class Main {
 
+    public static void Run() {
 
-delegate int intFn(int i, int j);
+        // create an instance of the publisher
+        EmergencyAlertCenter eac = new();
+        // subscribe to the message using
+        // both handlers
+        eac.AlertHandler += AlertHandler1;
+        eac.AlertHandler += AlertHandler2;
+        eac.Start();
+
+    }
+    // first handler to receive the message
+    public static void AlertHandler1(object sender, AlertArgs args) {
+        Console.WriteLine($"Handler1 ... Code[{args.Code}], Message[{args.Message}]");
+    }
+    // second handler to receive the message
+    public static void AlertHandler2(object sender, AlertArgs args) {
+        Console.WriteLine($"Handler2 ... ");
+    }
+}
+
+// thid defines what signature all handlers must have
+delegate void AlertEvent(object sender, AlertArgs args);
+
+// this class defines the custom data included
+// in the published message
+class AlertArgs : EventArgs {
+
+    public int Code { get; set; } = 0;
+    public string Message { get; set; } = null!;
+
+}
+
